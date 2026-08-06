0066e7879b2c036f04dd6b34e27e15128edcc7fe
Cris <lemora489@gmail.com>
2026-08-06 00:01:19 -0600

    Configuracion para Assure

--- Web/vite.config.js
+++ Web/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://purple-sky-006152910.7.azurestaticapps.net',
         changeOrigin: true,
       },
     },
